fce591f2e05a15fbe39caa7e3778789bfaa78e3e
di-octer <kojmitjp@gmail.com>
2026-05-27 01:37:08 +0900

    カリキュラムを見る人がmainを経由しない形へ

--- main.py
+++ main.py
@@ -4,40 +4,43 @@ import calculater
 
 def main(page: ft.Page):
     page.title = "Flet カリキュラム デモアプリ"
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    def go_todo(e):
+    def route_change(e):
         page.controls.clear()
-        ToDoApp.main(page)
+
+        if page.route == "/todo":
+            ToDoApp.main(page)
+        elif page.route == "/calc":
+            calculater.main(page)
+        else:
+            page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+            page.vertical_alignment = ft.MainAxisAlignment.CENTER
+            page.add(
+                ft.Text("Horizon Flet デモアプリ集", size=32, weight=ft.FontWeight.BOLD),
+                ft.Text("さわってみたいアプリを選んでください", size=16, color=ft.Colors.GREY),
+                ft.Container(height=20),
+
+                ft.Button(
+                    content=ft.Text("📝 Todoアプリを開く"),
+                    width=250,
+                    height=50,
+                    on_click=lambda _: page.go("/todo")
+                ),
+                
+                ft.Container(height=10),
+                
+                ft.Button(
+                    content=ft.Text("🔢 電卓アプリを開く"),
+                    width=250,
+                    height=50,
+                    on_click=lambda _: page.go("/calc")
+                ),
+            )
         page.update()
 
-    def go_calc(e):
-        page.controls.clear()
-        calculater.main(page)
-        page.update()
+    page.on_route_change = route_change
 
-    page.add(
-        ft.Text("Horizon Flet デモアプリ集", size=32, weight=ft.FontWeight.BOLD),
-        ft.Text("さわってみたいアプリを選んでください", size=16, color=ft.Colors.GREY),
-        ft.Container(height=20),
-        
-        ft.Button(
-            content=ft.Text("📝 Todoアプリを開く"),
-            width=250,
-            height=50,
-            on_click=go_todo
-        ),
-        
-        ft.Container(height=10),
-        
-        ft.Button(
-            content="🔢 電卓アプリを開く",
-            width=250,
-            height=50,
-            on_click=go_calc
-        ),
-    )
+    page.go(page.route)
 
 if __name__ == "__main__":
     ft.run(main)
